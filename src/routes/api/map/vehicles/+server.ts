ce0c702b410aebb3aec9427f3a41b42ff75b8af6
Trmntr4rzn <2cld2hndl@mghtspnk.ya>
2026-05-14 05:31:52 +0300

    Fix another broken import path

--- src/routes/api/map/vehicles/+server.ts
+++ src/routes/api/map/vehicles/+server.ts
@@ -1,7 +1,7 @@
 // src/routes/api/map/vehicles/+server.ts
 import { error } from '@sveltejs/kit';
-import { getMapService, distanceBetween, formatDistance} from '$lib/map/index.server';
-import {compressedJsonResponse, parseBounds } from '$lib/map/index.server';
+import { distanceBetween, formatDistance} from '$lib/map';
+import {compressedJsonResponse, parseBounds,getMapService } from '$lib/map/index.server';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ url, request }) => {
