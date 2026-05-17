da5a68c241f17330755836cd445c4ce51351b96b
Trmntr4rzn <2cld2hndl@mghtspnk.ya>
2026-05-17 20:15:27 +0300

    Moved from .com to .org

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const WebsiteName: string = "SXCNTCNQUNTS"
-export const WebsiteBaseUrl: string = "https://sxcntcnqunts.com"
+export const WebsiteBaseUrl: string = "https://sxcntcnqunts.org"
 export const WebsiteDescription: string =
   "Smart Matatu Management For Everyone.  FLAM is a powerful fleet management platform that helps commuters save time and operators improve service."
 export const CreateProfileStep: boolean = true
