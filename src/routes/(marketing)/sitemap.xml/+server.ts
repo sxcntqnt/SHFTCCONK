325e1b9c254fcb6ac0761746da3e1891d0fb6647
Trmntr4rzn <2cld2hndl@mghtspnk.ya>
2026-05-17 17:03:13 +0300

    Critical fix

--- src/routes/(marketing)/sitemap.xml/+server.ts
+++ src/routes/(marketing)/sitemap.xml/+server.ts
@@ -8,13 +8,14 @@ export const GET: RequestHandler = async () => {
   return sitemap.response({
     origin: WebsiteBaseUrl,
 
-    // Routes that should NEVER be included in the sitemap
     excludeRoutePatterns: [
-      ".*\\(auth\\).*",              // exclude standalone (auth)
-      ".*\\(marketing\\)/auth.*",    // exclude marketing auth routes
-      "^/verify(/.*)?$",            // /verify and subroutes
-      "^/login(/.*)?$",             // /login and subroutes
-      "^/login/invite/.*$",         // excludes /login/invite/[token]
+      ".*\\(auth\\).*",
+      ".*\\(marketing\\)/auth.*",
+      "^/verify(/.*)?$",
+      "^/login(/.*)?$",
+
+      // FIX
+      "^/login/invite/\\[token\\]$",
     ],
   })
 }
